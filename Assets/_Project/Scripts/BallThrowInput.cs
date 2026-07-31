98cf0e6c84e2450262f7d297f28056d17923c7ac
Burak Çifçi <burakcifci93@gmail.com>
2026-07-31 15:10:34 +0300

    v0.1-dev - Add basic physics throw prototype

---
+++ Assets/_Project/Scripts/BallThrowInput.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class BallThrowInput : MonoBehaviour
+{
+    [SerializeField] private BallThrower ballThrower;
+    [SerializeField] private float throwPower = 1f;
+
+    private void Update()
+    {
+        if (Keyboard.current == null) return;
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            ballThrower.Throw(Vector3.forward + Vector3.up * 0.8f, throwPower);
+        }
+
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/BallThrower.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallThrower : MonoBehaviour
+{
+    [SerializeField] private float forceMultiplier = 10f;
+
+    private Rigidbody rb;
+    private bool hasThrown;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Throw(Vector3 direction, float power)
+    {
+        if (hasThrown) return;
+
+        hasThrown = true;
+
+        Vector3 force = direction.normalized * power * forceMultiplier;
+        rb.AddForce(force, ForceMode.Impulse);
+    }
+}
